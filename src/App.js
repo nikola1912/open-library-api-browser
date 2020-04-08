310f6095cdabb6e0fbb50315ddba5ba6c2078071
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-04-08 17:16:58 +0200

    Install 'react-bootstrap' and 'bootstrap'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Button } from 'react-bootstrap';
 import TestComponent from './components/TestComponent.js';
 
 function App() {
   return (
-    <div className="App">
-      <TestComponent />
-    </div>
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+      <Button variant="link">Link</Button>
+    </>
   );
 }
 
